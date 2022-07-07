e304ee03a8d5ae8d304b61731990483e79b40fab
bamb1z <michelle.morilhas@gmail.com>
2022-07-06 23:47:02 -0300

    Utilizando react-router e reaproveitando componentes
    Criando novas páginas de Inicio, Sobre e Prato, reaproveitando componentes e estabelecendo as rotas.

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import Menu from 'components/Menu';
+import Footer from 'components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PaginaPadrao from 'components/PaginaPadrao';
+import Sobre from 'pages/Sobre';
+import Prato from  'pages/Prato';
+import NotFound from 'pages/NotFound';
+
+export default function AppRouter() {
+  return (
+    <main className={'container'}>
+      <Router>
+        <Menu />
+
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />} >
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
+          <Route path='prato/:id' element={<Prato />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main>
+  );
+} 
